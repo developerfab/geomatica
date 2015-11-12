e66b16e9c5271f10685bcd359bea6958acce5ccb
Cristian Fabricio Rodriguez Romero <fab7696650@gmail.com>
2015-11-11 21:12:35 -0500

    adicion de modelos basicos para la base de datos

--- geomatica/core/models.py
+++ geomatica/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Hueco(models.Model):
+    direccion = models.CharField(max_length=200)
+    latitud = models.IntegerField(default=0)
+    longitud = models.IntegerField(default=0)
+    foto = models.ImageField()
+
+class Trafico(models.Model):
+    zona = models.CharField(max_length=200)
+    nivel = models.IntegerField(default=1)
