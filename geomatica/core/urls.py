e551441fc5fce99dbdbeee405d8d56045b249d9c
Cristian Fabricio Rodriguez Romero <fab7696650@gmail.com>
2015-11-15 12:36:14 -0500

    se adiciona visualizacion de huecos en un punto

--- geomatica/core/urls.py
+++ geomatica/core/urls.py
@@ -4,4 +4,6 @@ from core import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index, name="index"),
+    url(r'^notificacion/$', views.notificacion, name="notificacion"),
+    url(r'^add_hueco/$', views.add_hueco, name="enviar_notificacion_hueco"),
     )
--- geomatica/core/views.py
+++ geomatica/core/views.py
@@ -6,5 +6,19 @@ def index(request):
     """
     Ventana principal
     """
-    contexto = {}
+    contexto = {'title':'BogoGIS'}
     return render(request, 'index.html', contexto)
+
+def notificacion(request):
+	"""
+	Metodo para visualizar la lista de notificaciones
+	"""
+	contexto = { "title":"BogoGIS-Notificacion", "latitud": 4.6282536, "longitud": -74.0657988 }
+	return render(request, 'map_location.html', contexto)
+
+def add_hueco(request):
+	"""
+	Metodo para agregar y crear una notificacion de hueco
+	"""
+	contexto = { 'title':'BogoGIS-huecos'}
+	return render(request, 'enviar_hueco.html', contexto)
